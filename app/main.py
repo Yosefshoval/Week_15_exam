4a6be66f5386a1f02f25b6421bbd72c7dc6809fd
Yosef shoval <ys0542522504@gmail.com>
2026-01-29 10:05:47 +0200

    connect between endpoints to eact dal function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from db_init import init_database
+import dal
 
 app = FastAPI()
 
@@ -11,32 +12,65 @@ def health_check():
 
 @app.get("/q1/customers-credit-limit-outliers")
 def customers_credit_limit_outliers():
-    pass
+    try:
+        result = dal.get_customers_by_credit_limit_range()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
+
 
 @app.get("/q2/orders-null-comments")
 def orders_null_comments():
-    pass
+    try:
+        result = dal.get_orders_with_null_comments()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q3/customers-first-5")
 def customers_first_5():
-    pass
+    try:
+        result = dal.get_first_5_customers()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q4/payments-total-average")
 def payments_total_average():
-    pass
+    try:
+        result = dal.get_payments_total_and_average()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q5/employees-office-phone")
 def employees_office_phone():
-    pass
+    try:
+        result = dal.get_employees_with_office_phone()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q6/customers-shipping-dates")
 def customers_shipping_dates():
-    pass
+    try:
+        result = dal.get_customers_with_shipping_dates()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q7/customer-quantity-per-order")
 def customer_quantity_per_order():
-    pass
+    try:
+        result = dal.get_customer_quantity_per_order()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/q8/customers-payments-by-lastname-pattern")
 def customers_payments_by_lastname_pattern(pattern: str = "son"):
-    pass
+    try:
+        result = dal.get_customers_payments_by_lastname_pattern()
+        return {"status" : "ok", "result" : result}
+    except Exception as e:
+        return {"error": str(e)}
